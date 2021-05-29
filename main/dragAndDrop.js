2d3586ffbd6873ef9b2227b0ebec409a1e3ee663
Andrew Lam <andrewthecoder1@gmail.com>
2021-05-28 20:51:18 -0400

    Fourth Commit - Task Creation, Local Storage and Styling

--- main/dragAndDrop.js
+++ main/dragAndDrop.js
@@ -1,19 +1,22 @@
 import addGlobalEventListener from '../utils/addGlobalEventListener';
 
-
+// Function Invocation (on App start)
 export default function setupToDoList(transferTask){ 
 
     addGlobalEventListener('mousedown', '[data-task-element]', e => {
-        const selectedTask = e.target.closest('[data-task]')
-        const taskClone = selectedTask.cloneNode(true);
-        const shadow = selectedTask.cloneNode();
+        const selectedTask = e.target.closest('[data-task]') //on click, will get selected element based on data attribute, [data-task] 
+        const taskClone = selectedTask.cloneNode(true); //cloneNode(true) clones 'selectedTask' and its child elements
+        const shadow = selectedTask.cloneNode(); 
+
+        // Runs setupDragItems and stores return value in variable to be used as an argument for 'setupDragEvents'
         const offset = setupDragItems(selectedTask, taskClone, shadow,e);
       
         setupDragEvents(selectedTask, taskClone, offset, shadow, transferTask)
-     
-
     })    
 }
+
+
+// Functions
 
 function setupDragItems(selectedTask, taskClone, shadow, e){
     const taskRect = selectedTask.getBoundingClientRect();
@@ -21,7 +24,6 @@ function setupDragItems(selectedTask, taskClone, shadow, e){
   const offset = {
     x: e.clientX - taskRect.left,
     y: e.clientY - taskRect.top
-
   }
 
     // Adding Class
@@ -57,35 +59,38 @@ function setupDragEvents(selectedTask, taskClone, offset,shadow, transferTask){
             return e.clientY < rect.top  + rect.height / 2
         })
 
-    
+        //closeChild represents the closest task after mouse hovers half way above a task
         if(closestChild != null){
+            // insert the shadow, before the closestChild
             dropArea.insertBefore(shadow, closestChild)
         } else {
             dropArea.append(shadow)
         }
         
     }
-
     
     document.addEventListener('mousemove', mouseMoveFunction)
     document.addEventListener('mouseup', e => {
         document.removeEventListener('mousemove', mouseMoveFunction)
         const dropArea = getDropArea(shadow);
         if(dropArea){
-            dropArea.insertBefore(selectedTask, shadow)
-
             transferTask({
                 startColumn: getDropArea(selectedTask),
                 endColumn: dropArea,
                 draggingTask: selectedTask,
                 index: Array.from(dropArea.children).indexOf(shadow)
             })
+
+            // insert must go after transferTask() for startColumn to use original state.
+            // this method will allow the shadow to be placed
+            dropArea.insertBefore(selectedTask, shadow)
         }
         removeDragElements(selectedTask, taskClone, shadow)
       
     }, { once: true })
 }
 
+// Utility Functions
 function removeDragElements(selectedTask, taskClone, shadow){
     selectedTask.classList.remove('hide')
     taskClone.remove()
@@ -93,6 +98,7 @@ function removeDragElements(selectedTask, taskClone, shadow){
 }
 
 function getDropArea(element){
+    // This function stops it from placing shadow anywhere, only in parent div of [data-task-column]
     // Detect and return shadow area of mouse target, where it hovers.
     if(element.matches('[data-task-column]')){
         return element
--- main/script.js
+++ main/script.js
@@ -1,7 +1,146 @@
 import setupToDoList from './dragAndDrop';
+import { v4 as uuidV4  } from 'uuid';
+
+
+const STORAGE_PREFIX = 'TODO_LIST'
+const STORAGE_KEY = `${STORAGE_PREFIX}_column`
+const form = document.querySelector('.form')
+const clearButton = document.querySelector('.clear-button')
+
+let DEFAULT_COLUMNS = {
+    tasksColumn: [],
+    completedTasksColumn: []
+}
+
+// will invoke loadColumns() once called in a function
+const columns = loadColumns()
+
+// Event Listeners
+
+form.addEventListener('submit', e=>{
+
+        e.preventDefault()
+        const taskHeader = document.querySelector('.task-subject').value
+        const taskContent = document.querySelector('.task-content').value
+        const inputFieldHeader = document.querySelector('.task-subject');
+        const inputFieldContent = document.querySelector('.task-content');
+    
+        if(taskHeader === '' || taskContent === '') return
+    
+        const task = { id: uuidV4(), taskHeader: taskHeader, taskContent: taskContent}
+        const columnName = 'tasksColumn'
+    
+        columns[columnName].push(task)
+    
+        saveColumns()
+        renderTasks()   
+    
+        inputFieldHeader.value ='';
+        inputFieldContent.value = '';  
+
+    
+})
+
+clearButton.addEventListener('click', ()=>{
+    clearColumns();
+})
+
+function clearColumns(){
+    const bothColumns = document.querySelectorAll('[data-task-column]')
+
+    bothColumns.forEach( col =>
+        col.innerHTML = ''
+        )
+    
+    // loop through object keys and turn pair into array
+    Object.entries(columns).forEach( obj => {
+        // each loop, will grab each array element's first element.
+        const columnName = obj[0]
+        columns[columnName] = []
+    })
+
+    saveColumns()
+}
+
+
+// Local Storage Utility Functions
+
+function saveColumns(){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(columns))
+}
+
+function loadColumns(){
+    //Retrieves local storage key item
+    const columnsJSON = localStorage.getItem(STORAGE_KEY)
+
+    //parse the JSON item if there are any (Truthy), if not, use default values.
+    return JSON.parse(columnsJSON) || DEFAULT_COLUMNS;
+}
+
+//General Functions
+
+function renderTasks(){
+    //columns is either DEFAULT_COLUMNS or the json object
+    Object.entries(columns).forEach( obj => {
+        //Loop through each object, obj[0] = taskColumn, completedTasksColumn
+        //obj[1] are the tasks, themselves
+        const columnName = obj[0]
+        const tasks = obj[1]
+        const column = document.querySelector(`[data-column-name="${columnName}"]`)
+
+        // clear column first
+        column.innerHTML = ''
+
+        tasks.forEach( task => {
+            const taskElement = createTaskElement(task)
+            column.append(taskElement)
+        })
+    })    
+}
+
+function createTaskElement(task){
+    // Task template div
+    const element = document.createElement('div');
+
+    // Data attributes
+    element.dataset.id = task.id
+    element.dataset.task = ''
+
+    // Template child elements
+    element.classList.add('task')
+    element.innerHTML =  
+        `
+            <div class="task-header" data-task-element>${task.taskHeader}</div>
+            <p data-task-element>${task.taskContent}</p>
+        `
+    return element
+}
+
+
+// Prop Transfer
+
+function transferTask(e){
+    // Retrieving the name of column
+    const startColumnName = e.startColumn.dataset.columnName;
+    const endColumnName = e.endColumn.dataset.columnName;
+    // Has to be bracket to use variable name; dot notation is used to access straight to the key.
+    const startColumnTasks = columns[startColumnName];
+    const endColumnTasks = columns[endColumnName];
+
+    const task = startColumnTasks.find(t => t.id == e.draggingTask.dataset.id)
+
+    //Find index of 'task' inside startColumnTasks, remove it
+    startColumnTasks.splice(startColumnTasks.indexOf(task), 1);
+    //Put in task at the 'shadow's index position
+    endColumnTasks.splice(e.index, 0, task) 
+
+    saveColumns()
+}
+
+
+// Invoke
 
 setupToDoList(transferTask);
 
-function transferTask(e){
-    console.log(e)
-}
+renderTasks();
+
